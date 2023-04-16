9558d317c8971f17f9c306d0573808bb16d3e0f1
a.rahmati <a.rahmati@kianiranian.com>
2023-04-16 17:57:54 +0330

    Repository

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './db/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private repo: Repository<User>) {}
+}

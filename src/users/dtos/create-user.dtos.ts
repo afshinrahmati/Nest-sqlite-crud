04f6fc83c12a9c9310a24610bd816bd31ce39c48
a.rahmati <a.rahmati@kianiranian.com>
2023-04-16 17:25:38 +0330

    user validation

---
+++ src/users/dtos/create-user.dtos.ts
@@ -0,0 +1,9 @@
+import { IsEmail, IsString, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+  @IsString()
+  password: string;
+}
